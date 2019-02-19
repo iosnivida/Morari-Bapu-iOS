3364ff05d247fac40c632b4faf9e1edd781f65c5
Bhavin Chauhan <bhavin.9909@gmail.com>
2019-02-20 00:37:07 +0530

    20_02_2019

---
+++ Morari Bapu/Controller/Internet Connection/InternetConnectionVC.swift
@@ -0,0 +1,30 @@
+//
+//  InternetConnectionVC.swift
+//  Morari Bapu
+//
+//  Created by Bhavin Chauhan on 18/02/19.
+//  Copyright © 2019 Bhavin Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class InternetConnectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Morari Bapu/Controller/Up Coming Kathas/Up Coming Katha Details/KathaTimingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KathaTimingTableViewCell.swift
+//  Morari Bapu
+//
+//  Created by Bhavin Chauhan on 15/02/19.
+//  Copyright © 2019 Bhavin Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class KathaTimingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
